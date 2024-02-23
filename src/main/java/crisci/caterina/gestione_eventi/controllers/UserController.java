2d8e97eeda9197b90205f1313c3263133f353c64
Katiacrisci <katcrisci@gmail.com>
2024-02-23 16:52:53 +0100

    add participation endpoint

--- src/main/java/crisci/caterina/gestione_eventi/controllers/UserController.java
+++ src/main/java/crisci/caterina/gestione_eventi/controllers/UserController.java
@@ -1,4 +1,20 @@
 package crisci.caterina.gestione_eventi.controllers;
 
+import crisci.caterina.gestione_eventi.models.Event;
+import crisci.caterina.gestione_eventi.service.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+
+    @Autowired
+    private EventService eventService;
+
+    @PostMapping("/{userId}/{eventId}")
+    public Event participate(@PathVariable Long userId, @PathVariable Long eventId) {
+        return eventService.addParticipant(userId, eventId);
+    }
+
 }
